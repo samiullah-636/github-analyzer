c65f81562d9ace2b1e391020df90f8cd3c895b26
samiullah-636 <samiullaharain636@gmail.com>
2026-03-31 07:35:06 +0500

    feat: create /analyze endpoint

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const {getUserData} = require('./services/githubService');
+const {analyzeData} = require('./utils/analyzer')
 const app = express();
 const port = 3000;
 
@@ -8,12 +9,14 @@ app.get('/', (req,res)=>
     res.send("API working fine");
 });
 
-app.get('/user/:username', async(req,res)=>
+app.get('/analyze/:username', async(req,res)=>
 {
 try{
     const {username} = req.params;
-const result = await getUserData(username);
+const {user,repos} = await getUserData(username);
+const result = analyzeData(user,repos);
  res.json(result);
+
 }
 catch (err) {
     res.status(404).json({ error: err.message });
